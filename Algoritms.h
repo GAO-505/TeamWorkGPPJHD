43732339ab3bd3c0fbc4a14465253e22f671809d
victoria9kina <victoria9kina@gmail.com>
2024-10-22 12:15:01 +0300

    Update Algoritms.h

--- Algoritms.h
+++ Algoritms.h
@@ -10,6 +10,6 @@ std::vector<int> sort_by_std(std::vector<int>);
 std::vector<int> shellsort_by_Name(std::vector<int> to_sort);
 std::vector<int> heapsort_by_Name(std::vector<int> to_sort);
 std::vector<int> merge_sort_by_Name(std::vector<int> data);
-std::vector<int> quick_sort_by_Name(std::vector<int> data);
+std::vector<int> quick_sort_by_Devyatkina(std::vector<int> data);
 //---------------------------------------------------------------
 
