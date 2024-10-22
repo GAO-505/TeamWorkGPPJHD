f95d41d5ef49ade563b7255241cc7ee37f58b32d
mariia029 <petrukhina.0@mail.ru>
2024-10-22 12:18:31 +0300

    Update Algoritms.h

--- Algoritms.h
+++ Algoritms.h
@@ -9,7 +9,7 @@ std::vector<int> sort_by_std(std::vector<int>);
 //--------------------Team Gazdik---------------------------
 std::vector<int> shellsort_by_Name(std::vector<int> to_sort);
 std::vector<int> heapsort_by_Name(std::vector<int> to_sort);
-std::vector<int> merge_sort_by_Name(std::vector<int> data);
+std::vector<int> merge_sort_by_Petrukhina(std::vector<int> data);
 std::vector<int> quick_sort_by_Devyatkina(std::vector<int> data);
 //---------------------------------------------------------------
 
