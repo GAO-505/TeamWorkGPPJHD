11494c681fb40d1dcead66d02ad9dd1709f0224b
Miko55562 <pudovninmiahil@gmail.com>
2024-10-29 02:06:00 +0300

    Fix(bitonic_sort_Pudovnin.cpp)

--- bitonic_sort_Pudovnin.cpp
+++ bitonic_sort_Pudovnin.cpp
@@ -1,37 +1,52 @@
 #include <vector>
 #include <algorithm>
+#include <climits>
+
 using namespace std;
 
-void compAndSwap(vector<int>& a, int i, int j) {
-    if (a[i] > a[j]) {
+void compAndSwap(vector<int>& a, int i, int j, bool dir) {
+    if (dir == (a[i] > a[j])) {
         swap(a[i], a[j]);
     }
 }
 
-void bitonicMerge(vector<int>& a, int low, int cnt) {
+void bitonicMerge(vector<int>& a, int low, int cnt, bool dir) {
     if (cnt > 1) {
         int k = cnt / 2;
         for (int i = low; i < low + k; i++) {
-            compAndSwap(a, i, i + k);
+            compAndSwap(a, i, i + k, dir);
         }
-        bitonicMerge(a, low, k);
-        bitonicMerge(a, low + k, k);
+        bitonicMerge(a, low, k, dir);
+        bitonicMerge(a, low + k, k, dir);
     }
 }
 
-void bitonicSort(vector<int>& a, int low, int cnt) {
+void bitonicSort(vector<int>& a, int low, int cnt, bool dir) {
     if (cnt > 1) {
         int k = cnt / 2;
 
-        bitonicSort(a, low, k);
-        bitonicSort(a, low + k, k);
-        bitonicMerge(a, low, cnt);
+        bitonicSort(a, low, k, true);
+        bitonicSort(a, low + k, k, false);
+        bitonicMerge(a, low, cnt, dir);
     }
 }
 
-vector<int> bitonic_sort_by_Pudovnin(vector<int> data) {
-    if (data.empty()) return data;
-    bitonicSort(data, 0, data.size());
-    return data;
+int nearestPowerOfTwo(int n) {
+    int power = 1;
+    while (power < n) {
+        power *= 2;
+    }
+    return power;
 }
 
+vector<int> bitonic_sort_by_Pudovnin(vector<int> data) {
+    int n = data.size();
+    int m = nearestPowerOfTwo(n);
+    data.resize(m, INT_MAX);
+
+    bitonicSort(data, 0, m, true);
+
+    data.erase(remove(data.begin(), data.end(), INT_MAX), data.end());
+
+    return data;
+}
